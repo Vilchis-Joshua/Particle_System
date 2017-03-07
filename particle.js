3537606615c4111bd6d1603e4aa442e482787678
crahman94 <christian.rahman@gmail.com>
2017-03-06 22:52:06 -0700

    hi
    newest version

--- particle.js
+++ particle.js
@@ -22,8 +22,8 @@ function init() {
 
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 6000);
-    camera.position.x = 400;
-    camera.position.y = 400;
+    camera.position.x = 1200;
+    camera.position.y = 800;
     camera.position.z = 1000;
 
     // var light = new THREE.DirectionalLight( 0xffffff );
@@ -44,22 +44,32 @@ function init() {
 
     window.addEventListener( 'resize', onWindowResize, false );
     controls = new THREE.OrbitControls(camera, renderer.domElement);
-    controls.maxDistance = 2500;
-    controls.minDistance = 300;
+    controls.maxDistance = 2000;
+    controls.minDistance = 600;
 
+    var particleSSun = createParticleSystem(0xEA7D17, 900000, "disc.png", 0, 0, 0, true, 1, 10, 80); //sun
+    var particleSSun1 = createParticleSystem(0xEEF21F, 100000, "disc.png", 0, 0, 0, true, 4, 80, 40); //sun
+    var particleSMercury = createParticleSystem(0x86989D, 10000, "disc.png", 110, 0, 0, true, 2, 1, 3); //mercury
+    var particleSVenus = createParticleSystem(0xDD9939, 30000, "disc.png", 140, 0, 0, true, 2, 1, 8); //venus
+    var particleSEarth = createParticleSystem(0x1E35BC, 30000, "disc.png", 180, 0, 0, true, 2, 1, 7); //earth
+    var particleSMars = createParticleSystem(0xDB5525 ,20000, "disc.png", 215, 0, 0, true, 2, 1, 4); //mars
+    var particleSJupiter = createParticleSystem(0xC1Ac8F, 60000, "particle.png", 280, 0, 0, true, 3, 1, 30); //jupiter
+    var particleSSaturn = createParticleSystem(0xC1B95B, 60000, "particle.png", 370, 0, 0, true, 3, 1, 27); //saturn
+    var particleSUranus = createParticleSystem(0x408BB7, 30000, "disc.png", 450, 0, 0, true, 2, 1, 17); //uranus
+    var particleSNeptune = createParticleSystem(0x408BB7, 40000, "particle.png", 500, 0, 0, true, 2, 1, 17); //neptune
+    var trialps = createParticleSystem1(0x1E35BC, 30000, "disc.png", -200, 0, 0, true, 2, 1, 7, 180, 180)
 
-    var particleSMercury = createParticleSystem(0x86989D, 100000, "disc.png", -100, 0, 0, true, 1, 5, 5);
-   //  var particleS2 = createParticleSystem(0x00ff00, 100000, "disc.png", 50, 0, 50, false, 2); //green
-   //  var particleS3 = createParticleSystem(0xff0000, 100000, "disc.png", -50, 0, -50, false, 2); //red
-  //  var sunLight = createLight(0x00ff00, -400, 0, -10);
-  //  scene.add( sunLight );
-    var particleSSun = createParticleSystem(0xEA7D17, 200000, "disc.png", -170, 0, 0, true, 4, 25, 40); //yellow
-
-   //  particleS2.position.set (150, 150, 150);
-    scene.add(particleSMercury);
-   //  scene.add(particleS2);
-   //  scene.add(particleS3);
     scene.add(particleSSun);
+    scene.add(particleSSun1);
+    scene.add(particleSMercury);
+    scene.add(particleSVenus);
+    scene.add(particleSEarth);
+    scene.add(particleSMars);
+    scene.add(particleSJupiter);
+    scene.add(particleSSaturn);
+    scene.add(particleSUranus);
+    scene.add(particleSNeptune);
+    scene.add(trialps);
     loadSkyBox();
 
     render();
@@ -116,33 +126,61 @@ function createParticleSystem(color, particleCount, image, startX, startY, start
     return particleSystem;
 }
 
-function animateParticles() {
-    var verts = particleSystem.geometry.vertices;
-    for(var i = 0; i < verts.length; i++) {
-        var vert = verts[i];
-        if (vert.y < -200) {
-            vert.y = Math.random() * 400 - 200;
-        }
-        vert.y = vert.y - (10 * deltaTime);
-    }
-    particleSystem.geometry.verticesNeedUpdate = true;
-    particleSystem.rotation.y -= .1 * deltaTime;
+function createParticleSystem1(color, particleCount, image, startX, startY, startZ, isTransparent, size, oRadius, iRadius ,j ,k) {
 
+    // The number of particles in a particle system is not easily changed.
+    // var particleCount = 10000;
+
+    // Particles are just individual vertices in a geometry
+    // Create the geometry that will hold all of the vertices
+    var particles = new THREE.Geometry();
+    var px = 0.0;
+    var py = 0.0;
+    var pz = 0.0;
+
+    // Create the vertices and add them to the particles geometry
+    for (var p = 0; p < particleCount; p++) {
+
+      var distance = THREE.Math.randFloatSpread(oRadius) - iRadius;
+      var theta = THREE.Math.randFloatSpread(j);
+      var phi = THREE.Math.randFloatSpread(k);
+
+      px = distance * Math.sin(theta) * Math.cos(phi);
+      py = distance * Math.sin(theta) * Math.sin(phi);
+      pz = distance * Math.cos(theta);
+      var particle = new THREE.Vector3(px, py, pz);
+        // // This will create all the vertices in a range of -200 to 200 in all directions
+        // var x = Math.random() * 400 - 200;
+        // var y = Math.random() * 400 - 200;
+        // var z = Math.random() * 400 - 200;
+        //
+        // // Create the vertex
+        // var particle = new THREE.Vector3(x, y, z);
+
+        // Add the vertex to the geometry
+        particles.vertices.push(particle);
+    }
+
+    // Create the material that will be used to render each vertex of the geometry
+    var particleMaterial = new THREE.PointsMaterial(
+            {color: color,
+             size: size,
+             map: THREE.ImageUtils.loadTexture(image),
+             blending: THREE.AdditiveBlending,
+             transparent: isTransparent,
+            });
+
+    // Create the particle system
+    particleSystem = new THREE.Points(particles, particleMaterial);
+
+    particleSystem.position.set (startX, startY, startZ);
+    return particleSystem;
 }
 
 function animate() {
-    // deltaTime = clock.getDelta();
 
-    // cubeMesh.rotation.x += 1 * deltaTime;
-    // cubeMesh.rotation.y += 2 * deltaTime;
-
-    //animateParticles();
     render();
     requestAnimationFrame( animate );
-
-    // rotationCamera += 0.01;
-    // camera.position.x = Math.sin(rotationCamera) * 100;
-    // camera.position.z = Math.cos(rotationCamera) * 100;
     camera.lookAt( scene.position ); // the origin
 }
 
@@ -172,28 +210,6 @@ function createLight( color, lx, ly, lz ) {
       return pointLight;
 }
 
-
-// function loadSkyBox() {
-//
-//      // Load the skybox images and create list of materials
-//      var materials = [
-//           createMaterial( 'images/skyX55+x.png' ), // right
-//           createMaterial( 'images/skyX55-x.png' ), // left
-//           createMaterial( 'images/skyX55+y.png' ), // top
-//           createMaterial( 'images/skyX55-y.png' ), // bottom
-//           createMaterial( 'images/skyX55+z.png' ), // back
-//           createMaterial( 'images/skyX55-z.png' )  // front
-//      ];
-//
-//      // Create a large cube
-//      var mesh = new THREE.Mesh( new THREE.BoxGeometry( 100, 100, 100, 1, 1, 1 ), new THREE.MeshFaceMaterial( materials ) );
-//
-//      // Set the x scale to be -1, this will turn the cube inside out
-//      mesh.scale.set(-1,1,1);
-//      scene.add( mesh );
-// }
-
-
 function loadSkyBox() {
 
         // Load the skybox images and create list of materials
@@ -215,33 +231,10 @@ function loadSkyBox() {
         scene.add( mesh );
 }
 
-function loadSkyBox2() {
-  var geometry = new THREE.SphereGeometry(300, 200, 200);
-  var materials = [
-      createMaterial( 'galaxy-wallpaper-02.jpg' ), // right
-
-  ];
-  var material = new THREE.MeshFaceMaterial( materials );
-// var uniforms = {
-//   texture: { type: 't', value: THREE.ImageUtils.loadTexture('galaxy-wallpaper-02.jpg') }
-// };
-
-// var material = new THREE.ShaderMaterial( {
-//   uniforms:       uniforms,
-//   vertexShader:   document.getElementById('sky-vertex').textContent,
-//   fragmentShader: document.getElementById('sky-fragment').textContent
-// });
-
-skyBox = new THREE.Mesh(geometry, material);
-skyBox.scale.set(-2, 2, 2);
-skyBox.eulerOrder = 'XZY';
-skyBox.renderDepth = 500.0;
-scene.add(skyBox);
-}
 
 function createMaterial( path ) {
     var texture = THREE.ImageUtils.loadTexture(path);
-    var material = new THREE.MeshBasicMaterial( { map: texture, overdraw: 0.5, doubleSided: true });
+    var material = new THREE.MeshBasicMaterial( { map: texture, overdraw: 0.5});
 
     return material;
 }
