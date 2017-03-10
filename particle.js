b2283796c2a32f80cb0e8b3dd04e9c6d85a49a89
crahman94 <christian.rahman@gmail.com>
2017-03-10 13:57:48 -0700

    Add files via upload

--- particle.js
+++ particle.js
@@ -42,17 +42,6 @@ function init() {
     camera.position.y = 800;
     camera.position.z = 1000;
 
-    // var light = new THREE.DirectionalLight( 0xffffff );
-    // light.position.set( 1, -1, 1 ).normalize();
-    // scene.add(light);
-
-    // var geometry = new THREE.CubeGeometry( 10, 10, 10);
-    // var material = new THREE.MeshPhongMaterial( { color: 0x000000, specular: 0x555555, shininess: 30 } );
-    //
-    // cubeMesh = new THREE.Mesh(geometry, material );
-    // cubeMesh.position.z = -30;
-    // scene.add( cubeMesh );
-
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
 
@@ -63,80 +52,51 @@ function init() {
     controls.maxDistance = 2000;
     controls.minDistance = 600;
 
-    particleSSun = createParticleSystem(0xEA7D17, 900000, "disc.png", 0, 0, 0, true, 1, 10, 80); //sun
-    particleSSun1 = createParticleSystem(0xEEF21F, 100000, "disc.png", 0, 0, 0, true, 4, 80, 40); //sun
-    particleSMercury = createParticleSystem(0x86989D, 10000, "disc.png", -110, 0, 0, true, 2, 1, 3); //mercury
-    particleSVenus = createParticleSystem(0xDD9939, 30000, "disc.png", 0, 0, -140, true, 2, 1, 8); //venus
-    particleSEarth = createParticleSystem(0x1E35BC, 30000, "disc.png", 0, 0, 180, true, 2, 1, 7); //earth
-    particleSMars = createParticleSystem(0xDB5525 ,20000, "disc.png", 215, 0, 0, true, 2, 1, 4); //mars
-    particleSJupiter = createParticleSystem(0xC1Ac8F, 60000, "particle.png", 0, 0, -280, true, 3, 1, 30); //jupiter
-    particleSSaturn = createParticleSystem(0xC1B95B, 60000, "particle.png", 0, 0, 370, true, 3, 1, 27); //saturn
-    particleSUranus = createParticleSystem(0x408BB7, 30000, "disc.png", -450, 0, 0, true, 2, 1, 17); //uranus
-    particleSNeptune = createParticleSystem(0x408BB7, 40000, "particle.png", 500, 0, 0, true, 2, 1, 17); //neptune
-    innerRingSaturn = createParticleSystemRing(0xEFBE5B, 10000, "disc.png", 0, 0, 370, true, 3, 12, 40, 360, 0);
-    outerRingSaturn = createParticleSystemRing(0xEFBE5B, 10000, "particle.png", 0, 0, 370, true, 3, 6, 51, 360, 0);
-
-    scene.add(particleSSun);
-    scene.add(particleSSun1);
-    scene.add(particleSMercury);
-    scene.add(particleSVenus);
-    scene.add(particleSEarth);
-    scene.add(particleSMars);
-    scene.add(particleSJupiter);
-    scene.add(particleSSaturn);
-    scene.add(particleSUranus);
-    scene.add(particleSNeptune);
-    scene.add(innerRingSaturn);
-    scene.add(outerRingSaturn);
-
-    //Sun
-    pivot = new THREE.Group();
-    //Mecury
-    pivotMecury = new THREE.Group();
-    pivotMecury.add( particleSMercury );
-    //Venus
-    pivotVenus = new THREE.Group();
-    pivotVenus.add( particleSVenus );
-    //Earth
-    pivotEarth = new THREE.Group();
-    pivotEarth.add( particleSEarth );
-    //Mars
-    pivotMars = new THREE.Group();
-    pivotMars.add( particleSMars );
-    //Jupiter
-    pivotJupiter = new THREE.Group();
-    scene.add( pivotJupiter );
-    pivotJupiter.add( particleSJupiter );
-    //Saturn
-    pivotSaturn = new THREE.Group();
-    pivotSaturn.add( particleSSaturn );
-    //Uranus
-    pivotUranus = new THREE.Group();
-    pivotUranus.add( particleSUranus );
-    //Neptune
-    pivotNeptune = new THREE.Group();
-    pivotNeptune.add( particleSNeptune );
-    //Saturn rings
-    pivotInnerRingSaturn = new THREE.Group();
-    pivotOuterRingSaturn = new THREE.Group();
-    pivotInnerRingSaturn.add ( innerRingSaturn )
-    pivotOuterRingSaturn.add( outerRingSaturn );
-
-    scene.add( pivot );
-    scene.add( pivotMecury );
-    scene.add( pivotVenus );
-    scene.add( pivotEarth );
-    scene.add( pivotMars );
-    scene.add( pivotSaturn );
-    scene.add( pivotUranus );
-    scene.add( pivotNeptune );
-    scene.add( pivotInnerRingSaturn );
-    scene.add( pivotOuterRingSaturn );
-
-
+    addSunAndPlanets();
+    addPivots();
     loadSkyBox();
 
+
     render();
+}
+
+function animate() {
+
+    render();
+    requestAnimationFrame( animate );
+   //  camera.lookAt( scene.position ); // the origin
+
+   animateParticles(particleSSun1);
+   rotatePlanets();
+   rotateAroundSun();
+   blowUpSun(particleSSun);
+   blowUpSun(particleSSun1);
+}
+
+
+function render() {
+    renderer.render( scene, camera );
+}
+
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    render();
+}
+
+// Create the lights function --------------------------------------------------
+function createLight( color, lx, ly, lz ) {
+      var pointLight = new THREE.PointLight( color, 1, 700 );
+      // var geometry = new THREE.SphereGeometry( 3, 12, 6 );
+      // var material = new THREE.MeshBasicMaterial( { color: color } );
+      //
+      // var sphere = new THREE.Mesh( geometry, material );
+      pointLight.position.set(lx, ly, lz);
+      pointLight.add( pointLight );
+
+      return pointLight;
 }
 
 function createParticleSystem(color, particleCount, image, startX, startY, startZ, isTransparent, size, oRadius, iRadius) {
@@ -241,62 +201,105 @@ function createParticleSystemRing(color, particleCount, image, startX, startY, s
     return particleSystem;
 }
 
-function animate() {
+function addSunAndPlanets() {
+  particleSSun = createParticleSystem(0xEA7D17, 700000, "disc.png", 0, 0, 0, true, 1, 10, 80); //sun
+  particleSSun1 = createParticleSystem(0xEEF21F, 100000, "disc.png", 0, 0, 0, true, 4, 80, 40); //sun
+  particleSMercury = createParticleSystem(0x86989D, 10000, "disc.png", -110, 0, 0, true, 2, 1, 3); //mercury
+  particleSVenus = createParticleSystem(0xDD9939, 30000, "disc.png", 0, 0, -140, true, 2, 1, 8); //venus
+  particleSEarth = createParticleSystem(0x1E35BC, 30000, "disc.png", 0, 0, 180, true, 2, 1, 7); //earth
+  particleSMars = createParticleSystem(0xDB5525 ,20000, "disc.png", 215, 0, 0, true, 2, 1, 4); //mars
+  particleSJupiter = createParticleSystem(0xC1Ac8F, 60000, "particle.png", 0, 0, -280, true, 3, 1, 30); //jupiter
+  particleSSaturn = createParticleSystem(0xC1B95B, 60000, "particle.png", 0, 0, 370, true, 3, 1, 27); //saturn
+  particleSUranus = createParticleSystem(0x408BB7, 30000, "disc.png", -450, 0, 0, true, 2, 1, 17); //uranus
+  particleSNeptune = createParticleSystem(0x408BB7, 40000, "particle.png", 500, 0, 0, true, 2, 1, 17); //neptune
+  innerRingSaturn = createParticleSystemRing(0xEFBE5B, 10000, "disc.png", 0, 0, 370, true, 3, 12, 40, 360, 0);
+  outerRingSaturn = createParticleSystemRing(0xEFBE5B, 10000, "particle.png", 0, 0, 370, true, 3, 6, 51, 360, 0);
 
-    render();
-    requestAnimationFrame( animate );
-   //  camera.lookAt( scene.position ); // the origin
-
-   animateParticles(particleSSun1);
-    particleSJupiter.rotation.y += 0.01;
-    particleSMercury.rotation.y += 0.01;
-    particleSVenus.rotation.y += 0.01;
-    particleSEarth.rotation.y += 0.01;
-    particleSMars.rotation.y += 0.01;
-    particleSJupiter.rotation.y += 0.01;
-    particleSSaturn.rotation.y += 0.01;
-    particleSUranus.rotation.y += 0.01;
-    particleSNeptune.rotation.y += 0.01;
-    outerRingSaturn.rotation.y += 0.01;
-    innerRingSaturn.rotation.y += 0.01;
-
-   //  pivot.rotation.y += 0.02;
-    pivotMecury.rotation.y += mecurySpeed;
-    pivotVenus.rotation.y += venusSpeed;
-    pivotEarth.rotation.y += earthSpeed;
-    pivotMars.rotation.y += marsSpeed;
-    pivotJupiter.rotation.y += jupiterSpeed;
-    pivotSaturn.rotation.y += saturnSpeed;
-    pivotUranus.rotation.y += uranusSpeed;
-    pivotNeptune.rotation.y += neptuneSpeed;
-    pivotOuterRingSaturn.rotation.y += outterRingSpeed;
-    pivotInnerRingSaturn.rotation.y += innerRingSpeed;
+  scene.add(particleSSun);
+  scene.add(particleSSun1);
+  scene.add(particleSMercury);
+  scene.add(particleSVenus);
+  scene.add(particleSEarth);
+  scene.add(particleSMars);
+  scene.add(particleSJupiter);
+  scene.add(particleSSaturn);
+  scene.add(particleSUranus);
+  scene.add(particleSNeptune);
+  scene.add(innerRingSaturn);
+  scene.add(outerRingSaturn);
 }
 
-
-function render() {
-    renderer.render( scene, camera );
+function rotateAroundSun() {
+  pivot.rotation.y += 0.02;
+  pivotMecury.rotation.y += mecurySpeed;
+  pivotVenus.rotation.y += venusSpeed;
+  pivotEarth.rotation.y += earthSpeed;
+  pivotMars.rotation.y += marsSpeed;
+  pivotJupiter.rotation.y += jupiterSpeed;
+  pivotSaturn.rotation.y += saturnSpeed;
+  pivotUranus.rotation.y += uranusSpeed;
+  pivotNeptune.rotation.y += neptuneSpeed;
+  pivotOuterRingSaturn.rotation.y += outterRingSpeed;
+  pivotInnerRingSaturn.rotation.y += innerRingSpeed;
 }
 
-
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    render();
+function rotatePlanets() {
+  particleSMercury.rotation.y += 0.058;
+  particleSVenus.rotation.y += 0.0116;
+  particleSEarth.rotation.y += 0.01;
+  particleSMars.rotation.y += 0.01;
+  particleSJupiter.rotation.y += 0.066;
+  particleSSaturn.rotation.y += 0.06;
+  particleSUranus.rotation.y += 0.03;
+  particleSNeptune.rotation.y += 0.03;
+  outerRingSaturn.rotation.y += 0.01;
+  innerRingSaturn.rotation.y += 0.01;
 }
 
-// Create the lights function --------------------------------------------------
-function createLight( color, lx, ly, lz ) {
-      var pointLight = new THREE.PointLight( color, 1, 700 );
-      // var geometry = new THREE.SphereGeometry( 3, 12, 6 );
-      // var material = new THREE.MeshBasicMaterial( { color: color } );
-      //
-      // var sphere = new THREE.Mesh( geometry, material );
-      pointLight.position.set(lx, ly, lz);
-      pointLight.add( pointLight );
+function addPivots() {
+  //Sun
+  pivot = new THREE.Group();
+  //Mecury
+  pivotMecury = new THREE.Group();
+  pivotMecury.add( particleSMercury );
+  //Venus
+  pivotVenus = new THREE.Group();
+  pivotVenus.add( particleSVenus );
+  //Earth
+  pivotEarth = new THREE.Group();
+  pivotEarth.add( particleSEarth );
+  //Mars
+  pivotMars = new THREE.Group();
+  pivotMars.add( particleSMars );
+  //Jupiter
+  pivotJupiter = new THREE.Group();
+  scene.add( pivotJupiter );
+  pivotJupiter.add( particleSJupiter );
+  //Saturn
+  pivotSaturn = new THREE.Group();
+  pivotSaturn.add( particleSSaturn );
+  //Uranus
+  pivotUranus = new THREE.Group();
+  pivotUranus.add( particleSUranus );
+  //Neptune
+  pivotNeptune = new THREE.Group();
+  pivotNeptune.add( particleSNeptune );
+  //Saturn rings
+  pivotInnerRingSaturn = new THREE.Group();
+  pivotOuterRingSaturn = new THREE.Group();
+  pivotInnerRingSaturn.add ( innerRingSaturn )
+  pivotOuterRingSaturn.add( outerRingSaturn );
 
-      return pointLight;
+  scene.add( pivot );
+  scene.add( pivotMecury );
+  scene.add( pivotVenus );
+  scene.add( pivotEarth );
+  scene.add( pivotMars );
+  scene.add( pivotSaturn );
+  scene.add( pivotUranus );
+  scene.add( pivotNeptune );
+  scene.add( pivotInnerRingSaturn );
+  scene.add( pivotOuterRingSaturn );
 }
 
 function loadSkyBox() {
@@ -347,9 +350,24 @@ function animateParticles(particleSystems) {
 
     var theta = THREE.Math.randFloatSpread(360);
     var phi = THREE.Math.randFloatSpread(360);
-    vert.x = vert.x + Math.sin(theta) * Math.cos(phi);
-    vert.y = vert.y + Math.sin(theta) * Math.sin(phi);
-    vert.z = vert.z + Math.cos(theta);
+    vert.x = vert.x + Math.sin(theta) * Math.cos(phi) *2;
+    vert.y = vert.y + Math.sin(theta) * Math.sin(phi) *2;
+    vert.z = vert.z + Math.cos(theta) *2;
+  }
+  particleSystems.geometry.verticesNeedUpdate = true;
+}
+
+function blowUpSun(particleSystems) {
+  var verts = particleSystems.geometry.vertices;
+  for(var i = 0; i < verts.length; i++) {
+    var vert = verts[i];
+
+
+    var theta = THREE.Math.randFloatSpread(360);
+    var phi = THREE.Math.randFloatSpread(360);
+    vert.x = vert.x + Math.sin(theta) * Math.cos(phi) * 100;
+    vert.y = vert.y + Math.sin(theta) * Math.sin(phi) * 100;
+    vert.z = vert.z + Math.cos(theta) * 100;
   }
   particleSystems.geometry.verticesNeedUpdate = true;
 }
