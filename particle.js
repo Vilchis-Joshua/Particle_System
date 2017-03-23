17649c7df485d0c094caa6468df8a2c11095dbe1
crahman94 <christian.rahman@gmail.com>
2017-03-22 21:36:18 -0600

    gui commands, change rotation, change position

--- particle.js
+++ particle.js
@@ -20,16 +20,16 @@ var pathMercury, pathVenus, pathEarth, pathMars, pathJupiter, pathSaturn, pathUr
 var pointLight;
 
 //Speeds of the planets
-var mecurySpeed = 0.08;
-var venusSpeed = 0.06;
-var earthSpeed = 0.04;
-var marsSpeed = 0.02;
-var jupiterSpeed = 0.01;
-var saturnSpeed = 0.009;
+var mecurySpeed = 0.00011364;
+var venusSpeed = 0.00004444;
+var earthSpeed = 0.0000274;
+var marsSpeed = 0.000014556;
+var jupiterSpeed = 0.000002308;
+var saturnSpeed = 0.000000929;
 var innerRingSpeed = saturnSpeed;
 var outterRingSpeed = saturnSpeed;
-var uranusSpeed = 0.007;
-var neptuneSpeed = 0.005;
+var uranusSpeed = 0.000000326;
+var neptuneSpeed = 0.000000166;
 
 init();
 animate();
@@ -53,42 +53,32 @@ function init() {
     window.addEventListener( 'resize', onWindowResize, false );
     controls = new THREE.OrbitControls(camera, renderer.domElement);
     controls.maxDistance = 2000;
-    //controls.minDistance = 600;
-
-  // var cubeGeometry = new THREE.CubeGeometry( 10, 10, 10, 30, 30, 30 );
-  // var triangleGeometry = new THREE.Geometry();
-  // triangleGeometry.vertices.push(new THREE.Vector3( 0.0,  20.0, 0.0));
-  // triangleGeometry.vertices.push(new THREE.Vector3(-20.0, -20.0, 0.0));
-  // triangleGeometry.vertices.push(new THREE.Vector3( 20.0, -20.0, 0.0));
-  // triangleGeometry.faces.push(new THREE.Face3(0, 1, 2));
-
-
-
-
-
-// var triangleMaterial = new THREE.MeshBasicMaterial({
-//color:0xFFFFFF,
-//side:THREE.DoubleSide
-//});
-
-// var triangleMesh = new THREE.Mesh(triangleGeometry, discTexture);
-// triangleMesh.position.set(0, 0.0, -400);
-// scene.add(triangleMesh);
+    controls.minDistance = 600;
 
 
     parameters = new function () {
       this.rotatoP = 1;
+      this.ShowPath = false;
+      this.showMercury = false;
+      this.showVenus = false;
+      this.showEarth = false;
+      this.showMars = false;
+      this.showJupiter = false;
+      this.showSaturn = false;
+      this.showUranus = false;
+      this.showNeptune = false;
       //this.reset = function () { reset() };
     };
 
     addParameters(parameters);
 
 
-    addPaths();
+    //addPaths();
     addSunAndPlanets();
     addMoons();
     addContinents();
     addPivots();
+
 
 
 
@@ -128,6 +118,8 @@ function onWindowResize() {
 // function reset() {
 //   init();
 // }
+
+
 
 function createTriangle3() {
   var x = 0, y = 0;
@@ -181,10 +173,186 @@ heartShape.bezierCurveTo( 9/4, -5/4, 7/4 ,0);
 
 function addParameters(param) {
   var gui = new dat.GUI();
-  gui.add(param, 'rotatoP', 0, 2, .1);
-  //gui.add(param, 'reset');
+  gui.add(param, 'rotatoP', 0, 10000, 1).name('Rotation Modifier');
+  var pathCont = gui.add(param, 'ShowPath').name('Path');
+  pathCont.onChange(function(value) {
+    if(value == true) {
+      addPaths();
+    }
+    else {
+      scene.remove(pathMercury);
+      scene.remove(pathVenus);
+      scene.remove(pathEarth);
+      scene.remove(pathMars);
+      scene.remove(pathJupiter);
+      scene.remove(pathSaturn);
+      scene.remove(pathUranus);
+      scene.remove(pathNeptune);
+    }
+}
+);
+    var f1 = gui.addFolder('Planets');
 
+    var mercuryCont = f1.add(param, 'showMercury').name('Show Mercury');
+    mercuryCont.onChange(function(value) {
+      if (value == true) {
+        //camera.lookAt( particleSEarth.position );
+        pivotMecury.add(camera);
 
+        camera.lookAt(particleSMercury.position);
+        camera.position.set(300, 200, 300);
+      }
+      else {
+        pivotMecury.remove(camera);
+        camera.position.x = 1200;
+        camera.position.y = 800;
+        camera.position.z = 1000;
+        camera.lookAt( scene.position );
+
+      }
+    }
+  );
+  var venusCont = f1.add(param, 'showVenus').name('Show Venus');
+  venusCont.onChange(function(value) {
+    if (value == true) {
+      //camera.lookAt( particleSEarth.position );
+      pivotVenus.add(camera);
+
+      camera.lookAt(particleSVenus.position);
+      camera.position.set(300, 200, 300);
+    }
+    else {
+      pivotVenus.remove(camera);
+      camera.position.x = 1200;
+      camera.position.y = 800;
+      camera.position.z = 1000;
+      camera.lookAt( scene.position );
+
+    }
+  }
+);
+
+    var earthCont = f1.add(param, 'showEarth').name('Show Earth');
+    earthCont.onChange(function(value) {
+      if (value == true) {
+        //camera.lookAt( particleSEarth.position );
+        pivotEarth.add(camera);
+
+        camera.lookAt(particleSEarth.position);
+        camera.position.set(300, 200, 350);
+      }
+      else {
+        pivotEarth.remove(camera);
+        camera.position.x = 1200;
+        camera.position.y = 800;
+        camera.position.z = 1000;
+        camera.lookAt( scene.position );
+
+      }
+    }
+  );
+
+  var marsCont = f1.add(param, 'showMars').name('Show Mars');
+  marsCont.onChange(function(value) {
+    if (value == true) {
+      //camera.lookAt( particleSEarth.position );
+      pivotMars.add(camera);
+
+      camera.lookAt(particleSMars.position);
+      camera.position.set(400, 200, 400);
+    }
+    else {
+      pivotMars.remove(camera);
+      camera.position.x = 1200;
+      camera.position.y = 800;
+      camera.position.z = 1000;
+      camera.lookAt( scene.position );
+
+    }
+  }
+);
+
+var jupiterCont = f1.add(param, 'showJupiter').name('Show Jupiter');
+jupiterCont.onChange(function(value) {
+  if (value == true) {
+    //camera.lookAt( particleSEarth.position );
+    pivotJupiter.add(camera);
+
+    camera.lookAt(particleSJupiter.position);
+    camera.position.set(500, 200, 500);
+  }
+  else {
+    pivotJupiter.remove(camera);
+    camera.position.x = 1200;
+    camera.position.y = 800;
+    camera.position.z = 1000;
+    camera.lookAt( scene.position );
+
+  }
+}
+);
+
+var saturnCont = f1.add(param, 'showSaturn').name('Show Saturn');
+saturnCont.onChange(function(value) {
+  if (value == true) {
+    //camera.lookAt( particleSEarth.position );
+    pivotSaturn.add(camera);
+
+    camera.lookAt(particleSSaturn.position);
+    camera.position.set(500, 250, 550);
+  }
+  else {
+    pivotSaturn.remove(camera);
+    camera.position.x = 1200;
+    camera.position.y = 800;
+    camera.position.z = 1000;
+    camera.lookAt( scene.position );
+
+  }
+}
+);
+
+var uranusCont = f1.add(param, 'showUranus').name('Show Uranus');
+uranusCont.onChange(function(value) {
+  if (value == true) {
+    //camera.lookAt( particleSEarth.position );
+    pivotUranus.add(camera);
+
+    camera.lookAt(particleSUranus.position);
+    camera.position.set(500, 300, 600);
+  }
+  else {
+    pivotUranus.remove(camera);
+    camera.position.x = 1200;
+    camera.position.y = 800;
+    camera.position.z = 1000;
+    camera.lookAt( scene.position );
+
+  }
+}
+);
+
+var neptuneCont = f1.add(param, 'showNeptune').name('Show Neptune');
+neptuneCont.onChange(function(value) {
+  if (value == true) {
+    //camera.lookAt( particleSEarth.position );
+    pivotNeptune.add(camera);
+
+    camera.lookAt(particleSNeptune.position);
+    camera.position.set(600, 300, 700);
+  }
+  else {
+    pivotNeptune.remove(camera);
+    camera.position.x = 1200;
+    camera.position.y = 800;
+    camera.position.z = 1000;
+    camera.lookAt( scene.position );
+
+  }
+}
+);
+
+  //f1.open();
   gui.open();
 }
 // Create the lights function --------------------------------------------------
@@ -302,48 +470,6 @@ function createParticleSystemRing(color, particleCount, image, startX, startY, s
     return particleSystem;
 }
 
-// function createTriangle(size, tcolor, posX, posY, posZ) {
-//   var x = 0, y = 0;
-//
-//   var heartShape = new THREE.Shape();
-//
-//   for(var i = 0; i < size; i++) {
-//     // heartShape.moveTo( x + 5, y + 5 );
-//     heartShape.bezierCurveTo(x + i, y + i);
-//     heartShape.bezierCurveTo(x - size + i, y + size - i);
-//     heartShape.bezierCurveTo(x + size - (2 * i), y + size);
-//   }
-//   var geometry = new THREE.ShapeGeometry( heartShape, 100);
-//
-// 	var discTexture = new THREE.PointCloudMaterial({color: tcolor});
-// 	// var particleMaterial = new THREE.ParticleBasicMaterial({ map: discTexture, size: 10, color: 0xff0000, transparency: true, alphaTest: 0.5});
-// 	var particleCube = new THREE.PointCloud( geometry, discTexture );
-// 	particleCube.position.set(posX, posY, posZ);
-//
-//   return particleCube;
-// 	//scene.add( particleCube );
-// }
-
-// function createTriangle2(size, tcolor, posX, posY, posZ) {
-//   var x = 0, y = 0;
-//
-//   var heartShape = new THREE.Shape();
-//
-//   for(var i = 0; i < size; i++) {
-//     // heartShape.moveTo( x + 5, y + 5 );
-//     heartShape.bezierCurveTo(x - i, y - i);
-//     heartShape.bezierCurveTo(x + size - i, y - size + i);
-//     heartShape.bezierCurveTo(x - size + (2 * i), y - size);
-//   }
-//   var geometry = new THREE.ShapeGeometry( heartShape );
-//
-// 	var discTexture = new THREE.PointCloudMaterial({color: tcolor});
-// 	var particleCube = new THREE.PointCloud( geometry, discTexture );
-// 	particleCube.position.set(posX, posY, posZ);
-//
-//   return particleCube;
-// }
-
 function addContinents() {
   na = createTriangle3();
 
@@ -356,9 +482,9 @@ function addPaths() {
   pathEarth = createParticleSystemRing(0xFFFFFF, 5000, "disc.png", 0, 0, 0, true, 1, 1, 180, 360, 0);
   pathMars = createParticleSystemRing(0xFFFFFF, 5000, "disc.png", 0, 0, 0, true, 1, 1, 215, 360, 0);
   pathJupiter = createParticleSystemRing(0xFFFFFF, 5000, "disc.png", 0, 0, 0, true, 1, 1, 280, 360, 0);
-  pathSaturn = createParticleSystemRing(0xFFFFFF, 5000, "disc.png", 0, 0, 0, true, 1, 1, 370, 360, 0);
-  pathUranus = createParticleSystemRing(0xFFFFFF, 5000, "disc.png", 0, 0, 0, true, 1, 1, 450, 360, 0);
-  pathNeptune = createParticleSystemRing(0xFFFFFF, 5000, "disc.png", 0, 0, 0, true, 1, 1, 500, 360, 0);
+  pathSaturn = createParticleSystemRing(0xFFFFFF, 8000, "disc.png", 0, 0, 0, true, 1, 1, 390, 360, 0);
+  pathUranus = createParticleSystemRing(0xFFFFFF, 10000, "disc.png", 0, 0, 0, true, 1, 1, 470, 360, 0);
+  pathNeptune = createParticleSystemRing(0xFFFFFF, 10000, "disc.png", 0, 0, 0, true, 1, 1, 520, 360, 0);
 
   scene.add(pathMercury);
   scene.add(pathVenus);
@@ -374,17 +500,17 @@ function addPaths() {
 function addSunAndPlanets() {
   particleSSun = createParticleSystem(0xEA7D17, 700000, "disc.png", 0, 0, 0, true, 1, 10, 80); //sun
   particleSSun1 = createParticleSystem(0xEEF21F, 100000, "disc.png", 0, 0, 0, true, 4, 80, 40); //sun
-  particleSMercury = createParticleSystem(0x86989D, 10000, "disc.png", -110, 0, 0, true, 2, 1, 3); //mercury
-  particleSVenus = createParticleSystem(0xDD9939, 30000, "disc.png", 0, 0, -140, true, 2, 1, 8); //venus
+  particleSMercury = createParticleSystem(0x86989D, 10000, "disc.png", 0, 0, 110, true, 2, 1, 3); //mercury
+  particleSVenus = createParticleSystem(0xDD9939, 30000, "disc.png", 0, 0, 140, true, 2, 1, 8); //venus
   particleSEarth = createParticleSystem(0x1E35BC, 30000, "particle.png", 0, 0, 180, true, 2, 1, 7); //earth
-  particleSMars = createParticleSystem(0xDB5525 ,20000, "disc.png", 215, 0, 0, true, 2, 1, 4); //mars
-  particleSJupiter = createParticleSystem(0xC1Ac8F, 60000, "particle.png", 0, 0, -280, true, 2, 3, 30); //jupiter
-  particleSSaturn = createParticleSystem(0xC1B95B, 60000, "particle.png", 0, 0, 370, true, 3, 1, 27); //saturn
-  particleSUranus = createParticleSystem(0x408BB7, 30000, "disc.png", -450, 0, 0, true, 2, 1, 17); //uranus
-  particleSNeptune = createParticleSystem(0x408BB7, 40000, "particle.png", 500, 0, 0, true, 2, 1, 17); //neptune
-  innerRingSaturn = createParticleSystemRing(0xEFBE5B, 10000, "disc.png", 0, 0, 370, true, 3, 12, 40, 360, 0);
-  outerRingSaturn = createParticleSystemRing(0xEFBE5B, 10000, "particle.png", 0, 0, 370, true, 3, 6, 51, 360, 0);
-  ringJupiter = createParticleSystemRing(0xEFBE5B, 1000, "particle.png", 0, 0, -280, true, 3, 12, 35, 360, 0);
+  particleSMars = createParticleSystem(0xDB5525 ,20000, "disc.png", 0, 0, 215, true, 2, 1, 4); //mars
+  particleSJupiter = createParticleSystem(0xC1Ac8F, 60000, "particle.png", 0, 0, 280, true, 2, 3, 30); //jupiter
+  particleSSaturn = createParticleSystem(0xC1B95B, 60000, "particle.png", 0, 0, 390, true, 3, 1, 27); //saturn
+  particleSUranus = createParticleSystem(0x408BB7, 30000, "disc.png", 0, 0, 470, true, 2, 1, 17); //uranus
+  particleSNeptune = createParticleSystem(0x408BB7, 40000, "particle.png", 0, 0, 520, true, 2, 1, 17); //neptune
+  innerRingSaturn = createParticleSystemRing(0xEFBE5B, 10000, "disc.png", 0, 0, 390, true, 3, 12, 40, 360, 0);
+  outerRingSaturn = createParticleSystemRing(0xEFBE5B, 10000, "particle.png", 0, 0, 390, true, 3, 6, 51, 360, 0);
+  ringJupiter = createParticleSystemRing(0xEFBE5B, 1000, "particle.png", 0, 0, 280, true, 3, 12, 35, 360, 0);
   eyeOfTheTiget = createParticleSystemRing(0xc6910b, 10000, "particle.png", 0, 0, -24.5, true, 2, 0, 6, 1, 360);
 
 
@@ -407,14 +533,14 @@ function addSunAndPlanets() {
 
 function addMoons() {
   earthMoon = createParticleSystem(0x87939B, 10000, "particle.png", 0, 0, 12, true, 2, 1, 1);
-  neptuneMoon = createParticleSystem(0x87939B, 10000, "particle.png", 25, 0, 0, true, 2, 1, 0.5);
-  marsMoon1 = createParticleSystem(0x87939B, 10000, "particle.png", 9, 0, 0, true, 2, 1, 0.1);
-  marsMoon2 = createParticleSystem(0x87939B, 10000, "particle.png", -9, 0, 0, true, 2, 1, 0.1);
+  // neptuneMoon = createParticleSystem(0x87939B, 10000, "particle.png", 25, 0, 0, true, 2, 1, 0.5);
+  // marsMoon1 = createParticleSystem(0x87939B, 10000, "particle.png", 9, 0, 0, true, 2, 1, 0.1);
+  // marsMoon2 = createParticleSystem(0x87939B, 10000, "particle.png", -9, 0, 0, true, 2, 1, 0.1);
 
   scene.add(earthMoon);
-  scene.add(marsMoon1);
-  scene.add(marsMoon2);
-  scene.add(neptuneMoon);
+  // scene.add(marsMoon1);
+  // scene.add(marsMoon2);
+  // scene.add(neptuneMoon);
 }
 
 function rotateAroundSun() {
@@ -434,25 +560,25 @@ function rotateAroundSun() {
 
 function rotateMoons () {
 
-    pivotEarthMoon.rotation.y += 0.05;
-    pivotMarsMoon.rotation.y += 0.05;
-    pivotNeptuneMoon.rotation.y += 0.05;
+    pivotEarthMoon.rotation.y += (parameters.rotatoP * 0.000366);
+    // pivotMarsMoon.rotation.y -= 0.05;
+    // pivotNeptuneMoon.rotation.y -= 0.05;
 }
 
 function rotatePlanets() {
-  particleSMercury.rotation.y += 0.00017;
-  particleSVenus.rotation.y += 0.000086;
-  particleSEarth.rotation.y += 0.01;
-  particleSMars.rotation.y += 0.01;
-  particleSJupiter.rotation.y += 0.0266;
-  particleSSaturn.rotation.y += 0.024;
-  particleSUranus.rotation.y += 0.0141;
-  particleSNeptune.rotation.y += 0.015;
-  outerRingSaturn.rotation.y += 0.015;
-  innerRingSaturn.rotation.y += 0.02;
-  pivotEye.rotation.y += 0.0266;
-  pivotContinents.rotation.y += 0.01;
-  ringJupiter.rotation.y += 0.0255;
+  particleSMercury.rotation.y += (parameters.rotatoP * 0.00017);
+  particleSVenus.rotation.y += (parameters.rotatoP * 0.000086);
+  particleSEarth.rotation.y += (parameters.rotatoP * 0.01);
+  particleSMars.rotation.y += (parameters.rotatoP * 0.01);
+  particleSJupiter.rotation.y += (parameters.rotatoP * 0.0266);
+  particleSSaturn.rotation.y += (parameters.rotatoP * 0.024);
+  particleSUranus.rotation.y += (parameters.rotatoP * 0.0141);
+  particleSNeptune.rotation.y += (parameters.rotatoP * 0.015);
+  outerRingSaturn.rotation.y += (parameters.rotatoP * 0.015);
+  innerRingSaturn.rotation.y += (parameters.rotatoP * 0.02);
+  pivotEye.rotation.y += (parameters.rotatoP * 0.0266);
+  pivotContinents.rotation.y += (parameters.rotatoP * 0.01);
+  ringJupiter.rotation.y += (parameters.rotatoP * 0.0255);
 }
 
 function addPivots() {
@@ -477,18 +603,18 @@ function addPivots() {
   pivotEarth.add( pivotEarthMoon );
   pivotEarth.add( pivotContinents );
   //Mars
-  pivotMarsMoon = new THREE.Group();
-  pivotMarsMoon.position.set(215, 0, 0);
-  pivotMarsMoon.add( marsMoon1 );
-  pivotMarsMoon.add( marsMoon2 );
+  // pivotMarsMoon = new THREE.Group();
+  // pivotMarsMoon.position.set(0, 0, 215);
+  // pivotMarsMoon.add( marsMoon1 );
+  // pivotMarsMoon.add( marsMoon2 );
 
   pivotMars = new THREE.Group();
   pivotMars.add( particleSMars );
-  pivotMars.add( pivotMarsMoon );
+  // pivotMars.add( pivotMarsMoon );
   //Jupiter
 
   pivotEye = new THREE.Group();
-  pivotEye.position.set(0, -10, -280);
+  pivotEye.position.set(0, -10, 280);
   pivotEye.add ( eyeOfTheTiget );
 
   pivotJupiter = new THREE.Group();
@@ -506,13 +632,13 @@ function addPivots() {
   pivotUranus.add( particleSUranus );
   //Neptune
 
-  pivotNeptuneMoon = new THREE.Group();
-  pivotNeptuneMoon.position.set(500, 0, 0);
-  pivotNeptuneMoon.add( neptuneMoon );
+  // pivotNeptuneMoon = new THREE.Group();
+  // pivotNeptuneMoon.position.set(0, 0, 520);
+  // pivotNeptuneMoon.add( neptuneMoon );
 
   pivotNeptune = new THREE.Group();
   pivotNeptune.add( particleSNeptune );
-  pivotNeptune.add( pivotNeptuneMoon );
+  // pivotNeptune.add( pivotNeptuneMoon );
 
 
 
